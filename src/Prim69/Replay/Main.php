3fef25b5315adc7cdb19c8d52b025c8a5c86c93e
xJustJqy <78117567+xJustJqy@users.noreply.github.com>
2021-08-10 22:00:13 -0700

    Removed SkinData saving

--- src/Prim69/Replay/Main.php
+++ src/Prim69/Replay/Main.php
@@ -10,14 +10,8 @@ use pocketmine\network\mcpe\protocol\RequestChunkRadiusPacket;
 use pocketmine\network\mcpe\protocol\ResourcePackChunkDataPacket;
 use pocketmine\network\mcpe\protocol\ResourcePackChunkRequestPacket;
 use pocketmine\network\mcpe\protocol\TextPacket;
-use pocketmine\network\mcpe\protocol\LoginPacket;
 use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
-use pocketmine\network\mcpe\protocol\types\SkinImage;
-use pocketmine\network\mcpe\protocol\types\SkinData;
-use pocketmine\network\mcpe\protocol\types\SkinAnimation;
-use pocketmine\network\mcpe\protocol\types\PersonaPieceTintColor;
-use pocketmine\network\mcpe\protocol\types\PersonaSkinPiece;
 use pocketmine\event\block\BlockPlaceEvent;
 use pocketmine\event\block\BlockBreakEvent;
 use pocketmine\block\Block;
@@ -38,9 +32,6 @@ class Main extends PluginBase implements Listener
 
 	/** @var array */
 	public $positions = [];
-
-	/** @var array */
-	public $skinData = [];
 
 	public const IGNORE_SERVERBOUND = [
 		LevelChunkPacket::class,
@@ -89,67 +80,6 @@ class Main extends PluginBase implements Listener
 				$this->recording[$player->getName()]["packets"][(string) round(microtime(true), 2)] = $pk;
 			}
 		}
-		if ($pk instanceof LoginPacket) {
-			$packet = $pk;
-			$animations = [];
-			foreach ($packet->clientData["AnimatedImageData"] as $animation) {
-				$animations[] = new SkinAnimation(
-					new SkinImage(
-						$animation["ImageHeight"],
-						$animation["ImageWidth"],
-						base64_decode($animation["Image"], true)
-					),
-					$animation["Type"],
-					$animation["Frames"],
-					$animation["AnimationExpression"]
-				);
-			}
-
-			$personaPieces = [];
-			foreach ($packet->clientData["PersonaPieces"] as $piece) {
-				$personaPieces[] = new PersonaSkinPiece(
-					$piece["PieceId"],
-					$piece["PieceType"],
-					$piece["PackId"],
-					$piece["IsDefault"],
-					$piece["ProductId"]
-				);
-			}
-
-			$pieceTintColors = [];
-			foreach ($packet->clientData["PieceTintColors"] as $tintColor) {
-				$pieceTintColors[] = new PersonaPieceTintColor($tintColor["PieceType"], $tintColor["Colors"]);
-			}
-			$skinData = new SkinData(
-				$packet->clientData["SkinId"],
-				$packet->clientData["PlayFabId"],
-				base64_decode($packet->clientData["SkinResourcePatch"] ?? "", true),
-				new SkinImage(
-					$packet->clientData["SkinImageHeight"],
-					$packet->clientData["SkinImageWidth"],
-					base64_decode($packet->clientData["SkinData"], true)
-				),
-				$animations,
-				new SkinImage(
-					$packet->clientData["CapeImageHeight"],
-					$packet->clientData["CapeImageWidth"],
-					base64_decode($packet->clientData["CapeData"] ?? "", true)
-				),
-				base64_decode($packet->clientData["SkinGeometryData"] ?? "", true),
-				base64_decode($packet->clientData["SkinAnimationData"] ?? "", true),
-				$packet->clientData["PremiumSkin"] ?? false,
-				$packet->clientData["PersonaSkin"] ?? false,
-				$packet->clientData["CapeOnClassicSkin"] ?? false,
-				$packet->clientData["CapeId"] ?? "",
-				null,
-				$packet->clientData["ArmSize"] ?? SkinData::ARM_SIZE_WIDE,
-				$packet->clientData["SkinColor"] ?? "",
-				$personaPieces,
-				$pieceTintColors,
-				true
-			);
-			if(!!$packet->xuid) $this->skinData[$packet->xuid] = $skinData;
-		}
 	}
 
 	public function onQuit(PlayerQuitEvent $event)
@@ -158,9 +88,6 @@ class Main extends PluginBase implements Listener
 		if ($this->isRecording($name)) {
 			$this->saved[$name] = $this->recording[$name];
 			unset($this->recording[$name]);
-		}
-		if (isset($this->skinData[$event->getPlayer()->getXuid()])) {
-			unset($this->skinData[$event->getPlayer()->getXuid()]);
 		}
 	}
 
