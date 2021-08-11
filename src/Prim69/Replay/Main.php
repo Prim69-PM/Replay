69a6a9ddd7acee8aa405c7c957d429f8593e3a0a
xJustJqy <78117567+xJustJqy@users.noreply.github.com>
2021-08-10 20:41:53 -0700

    Added saving of player's SkinData upon logging in

--- src/Prim69/Replay/Main.php
+++ src/Prim69/Replay/Main.php
@@ -10,14 +10,22 @@ use pocketmine\network\mcpe\protocol\RequestChunkRadiusPacket;
 use pocketmine\network\mcpe\protocol\ResourcePackChunkDataPacket;
 use pocketmine\network\mcpe\protocol\ResourcePackChunkRequestPacket;
 use pocketmine\network\mcpe\protocol\TextPacket;
+use pocketmine\network\mcpe\protocol\LoginPacket;
 use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
+use pocketmine\network\mcpe\protocol\types\SkinImage;
+use pocketmine\network\mcpe\protocol\types\SkinData;
+use pocketmine\network\mcpe\protocol\types\SkinAnimation;
+use pocketmine\network\mcpe\protocol\types\PersonaPieceTintColor;
+use pocketmine\network\mcpe\protocol\types\PersonaSkinPiece;
+use pocketmine\utils\UUID;
 use function get_class;
 use function in_array;
 use function microtime;
 use function round;
 
-class Main extends PluginBase implements Listener {
+class Main extends PluginBase implements Listener
+{
 
 	/** @var array */
 	public $recording = [];
@@ -28,6 +36,9 @@ class Main extends PluginBase implements Listener {
 	/** @var array */
 	public $positions = [];
 
+	/** @var array */
+	public $skinData = [];
+
 	public const IGNORE_SERVERBOUND = [
 		LevelChunkPacket::class,
 		RequestChunkRadiusPacket::class,
@@ -36,38 +47,106 @@ class Main extends PluginBase implements Listener {
 		TextPacket::class
 	];
 
-	public function onEnable(){
+	public function onEnable()
+	{
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
 		$this->getServer()->getCommandMap()->register($this->getName(), new ReplayCommand($this));
 	}
 
-	public function showRecording(Player $player, Player $target) {
+	public function showRecording(Player $player, Player $target)
+	{
 		$this->getScheduler()->scheduleRepeatingTask(new ReplayTask($player, $target, $this), 1);
 	}
 
-	public function isRecording(string $name) : bool {
+	public function isRecording(string $name): bool
+	{
 		return isset($this->recording[$name]);
 	}
 
-	public function onReceive(DataPacketReceiveEvent $event){
+	public function onReceive(DataPacketReceiveEvent $event)
+	{
 		$pk = $event->getPacket();
 		$player = $event->getPlayer();
-		if($this->isRecording($player->getName())){
-			if(!in_array(get_class($pk), self::IGNORE_SERVERBOUND)){
+		if ($this->isRecording($player->getName())) {
+			if (!in_array(get_class($pk), self::IGNORE_SERVERBOUND)) {
 				$this->recording[$player->getName()][(string) round(microtime(true), 2)] = $pk;
 			}
 		}
-	}
+		if ($pk instanceof LoginPacket) {
+			$packet = $pk;
+			$animations = [];
+			foreach ($packet->clientData["AnimatedImageData"] as $animation) {
+				$animations[] = new SkinAnimation(
+					new SkinImage(
+						$animation["ImageHeight"],
+						$animation["ImageWidth"],
+						base64_decode($animation["Image"], true)
+					),
+					$animation["Type"],
+					$animation["Frames"],
+					$animation["AnimationExpression"]
+				);
+			}
 
-	public function onQuit(PlayerQuitEvent $event){
-		$name = $event->getPlayer()->getName();
-		if($this->isRecording($name)){
-			$this->saved[$name] = $this->recording[$name];
-			unset($this->recording[$name]);
+			$personaPieces = [];
+			foreach ($packet->clientData["PersonaPieces"] as $piece) {
+				$personaPieces[] = new PersonaSkinPiece(
+					$piece["PieceId"],
+					$piece["PieceType"],
+					$piece["PackId"],
+					$piece["IsDefault"],
+					$piece["ProductId"]
+				);
+			}
+
+			$pieceTintColors = [];
+			foreach ($packet->clientData["PieceTintColors"] as $tintColor) {
+				$pieceTintColors[] = new PersonaPieceTintColor($tintColor["PieceType"], $tintColor["Colors"]);
+			}
+			$skinData = new SkinData(
+				$packet->clientData["SkinId"],
+				$packet->clientData["PlayFabId"],
+				base64_decode($packet->clientData["SkinResourcePatch"] ?? "", true),
+				new SkinImage(
+					$packet->clientData["SkinImageHeight"],
+					$packet->clientData["SkinImageWidth"],
+					base64_decode($packet->clientData["SkinData"], true)
+				),
+				$animations,
+				new SkinImage(
+					$packet->clientData["CapeImageHeight"],
+					$packet->clientData["CapeImageWidth"],
+					base64_decode($packet->clientData["CapeData"] ?? "", true)
+				),
+				base64_decode($packet->clientData["SkinGeometryData"] ?? "", true),
+				base64_decode($packet->clientData["SkinAnimationData"] ?? "", true),
+				$packet->clientData["PremiumSkin"] ?? false,
+				$packet->clientData["PersonaSkin"] ?? false,
+				$packet->clientData["CapeOnClassicSkin"] ?? false,
+				$packet->clientData["CapeId"] ?? "",
+				null,
+				$packet->clientData["ArmSize"] ?? SkinData::ARM_SIZE_WIDE,
+				$packet->clientData["SkinColor"] ?? "",
+				$personaPieces,
+				$pieceTintColors,
+				true
+			);
+			if(!!$packet->xuid) $this->skinData[$packet->xuid] = $skinData;
 		}
 	}
 
-	// dont feel like finishing this
+	public function onQuit(PlayerQuitEvent $event)
+	{
+		$name = $event->getPlayer()->getName();
+		if ($this->isRecording($name)) {
+			$this->saved[$name] = $this->recording[$name];
+			unset($this->recording[$name]);
+		}
+		if (isset($this->skinData[$event->getPlayer()->getXuid()])) {
+			unset($this->skinData[$event->getPlayer()->getXuid()]);
+		}
+	}
+
 	/*public function throwFakeProjectile(Player $player, ?ProjectileItem $item, Location $l){
 		$pk = new AddActorPacket();
 		$pk->position = $l;
@@ -87,5 +166,4 @@ class Main extends PluginBase implements Listener {
 
 		$player->dataPacket($pk);
 	}*/
-
 }
